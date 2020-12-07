991acdc20558438a460ad465bad9993b0b05415d
Austin Lilly <austinlilly15@gmail.com>
2020-12-07 16:46:13 -0600

    modified the gitignore file

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,44 @@
   <div id="app">
 
     <Header theTitle="Popular TV Shows"/>
+    <TaskList theTitle = "My Tasks" :taskList="tasks"/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import TaskList from './components/Card.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    Header
-  }
+    Header, 
+    TaskList
+  },
+  data()
+  {
+    return {
+      tasks: [
+        //{id:1, title: "Learns HTML & CSS", completed: false},
+        //{id:2, title: "Learn Javascript", completed: false},
+        //{id:3, title: "Learn a JS Framework", completed: false}
+      ]
+    }
+  },
+  mounted()
+    {
+
+        axios.get('https://api.themoviedb.org/3/tv/popular?api_key=571018b708e3c5a431f7569452cb4421&language=en-US&page=1')
+        
+        //https://api.themoviedb.org/3/movie/now_playing?api_key=571018b708e3c5a431f7569452cb4421&language=en-US&page=1')
+
+
+        .then( (response)=> {
+            this.tasks = response.data.results.slice(0,4);
+        })
+
+    }
 }
 </script>
 
---
+++ src/components/Card.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="center">
+        <div v-for="task in taskList" :key="task.id" class="container">
+            <img v-bind:src="'https://image.tmdb.org/t/p/w300//' + task.poster_path">
+            <h1>{{task.name}}</h1>
+            <p>{{task.overview}}</p>
+        </div>
+        
+        <Task :aTask="task" /> 
+    </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+export default {
+    name:"TaskList",
+    props:['theTitle', 'taskList'],
+    components: {
+        Task
+    }
+}
+</script>
+
+<style scoped>
+
+.center {
+    display: flex;
+    justify-content: center;
+    padding: 250px;
+}
+
+.container {
+    max-width: 25%;
+    background-color: wheat;
+    padding: 25px;
+    border-radius: 25px;
+    box-shadow: 0 0 100px 20px;
+}
+
+img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+h1 {
+    text-align: center;
+}
+
+</style>
